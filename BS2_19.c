0d035f56da7c77f7c03afe6a0df6ee9d28ae6734
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2018-10-14 19:27:21 +0530

    Update BS2_19.c

--- BS2_19.c
+++ BS2_19.c
@@ -3,9 +3,13 @@
 int main(){
 	int n,i,f=1;
 	scanf("%d",&n);
-	for(i=1;i<=n;i++){
-		f = f*i;
+	if(n==0 || n==1)
+		printf("1");
+	else{
+		for(i=1;i<=n;i++){
+			f = f*i;
+		}
+		printf("%d",f);
 	}
-	printf("%d",f);
 	return 0;
 }
