307de70c0fc07ef10e34576dea150e46388f12d5
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2018-10-14 17:11:17 +0530

    Update BS1_3.c

--- BS1_3.c
+++ BS1_3.c
@@ -5,7 +5,9 @@ int main(){
 	scanf("%c",&ch);
 	if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
 		printf("Vowel");
-	else
+	else if((ch>='b' && ch<='z') ||(ch>='B' && ch<='Z'))
 		printf("Consonant");
+	else
+		printf("Invalid");
 	return 0;
 }
